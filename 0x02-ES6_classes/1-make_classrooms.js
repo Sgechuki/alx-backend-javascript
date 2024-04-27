debe9df57a85e3f3a2c560e2275857e9ec05a28a
Sgechuki <sgechuki@outlook.com>
2024-04-27 01:47:06 +0000

    Fix eslint warning

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  let obj1 = new ClassRoom(19);
-  let obj2 = new ClassRoom(20);
-  let obj3 = new ClassRoom(34);
+  const obj1 = new ClassRoom(19);
+  const obj2 = new ClassRoom(20);
+  const obj3 = new ClassRoom(34);
   return ([obj1,
     obj2,
     obj3]
-  )
+  );
 }
