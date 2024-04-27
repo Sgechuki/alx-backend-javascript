18e031c96b85effacefbb705f15c0600e2bbb70a
Sgechuki <sgechuki@outlook.com>
2024-04-27 02:04:48 +0000

    Fix eslint errors

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,19 +3,24 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   get name() {
     return this._name;
   }
+
   set code(code) {
     this._code = code;
   }
+
   set name(name) {
     this._name = name;
   }
+
   displayFullCurrency() {
-    return `${this.name} (${this.code})`
+    return `${this.name} (${this.code})`;
   }
 }
