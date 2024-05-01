1a428af41cd11f0f7474e1aa633668dd8b6de5aa
Sgechuki <sgechuki@outlook.com>
2024-05-01 13:22:05 +0000

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,16 @@
 export default function cleanSet(set, startString) {
   let str = '';
-  if (startString) {
-    for (const elem of set) {
-      if (elem.startsWith(startString)) {
-        if (str.length) {
-          str += '-';
-        }
-        str += elem.slice(startString.length, -1);
+  if (startString === '' || typeof startString !== 'string') {
+    return str;
+  }
+  for (const elem of set) {
+    if (typeof elem === 'string' && elem.startsWith(startString)) {
+      if (str.length) {
+        str += '-';
       }
+      str += elem.slice(startString.length, -1);
     }
   }
+
   return str;
 }
